05a28db66f2225cc59260ae0c250894bdd8f44fe
Nicolas Kocher <nicokocher1999@gmail.com>
2025-08-25 18:46:36 +0200

    Initial Commit

---
+++ FastDo/FastDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastDoApp.swift
+//  FastDo
+//
+//  Created by Nicolas Kocher on 25.08.25.
+//
+
+import SwiftUI
+
+@main
+struct FastDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
